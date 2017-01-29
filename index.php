09b1b308583cfa304f167856edab230fc0304150
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-01-29 22:14:36 +0700

    load to session, just means that serialize/unserialize, after all, serialize FAIL

--- index.php
+++ index.php
@@ -1,24 +1,38 @@
 <?php
 ini_set('memory_limit','2048M');
+// simple clean session
+// $_SESSION = []; die;
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
 require 'vendor/autoload.php';
 
-// Open dict's info, index and data.
-$start = time();
-echo "{$start}\n";
-$info = new skoro\stardict\Info('dict/en_vi.ifo');
-var_dump($info);
-$index = new skoro\stardict\Index($info);
-// var_dump($index);
-$dict = new skoro\stardict\Dict($index);
-// var_dump($dict);
+function loadDict(){
+	// $_SESSION = [];
+	if(empty($_SESSION['dict'])){
+		echo "First load\n";
+		// Open dict's info, index and data.
+		$start = time();
+		
+		$info = new skoro\stardict\Info('dict/en_vi.ifo');
+		$index = new skoro\stardict\Index($info);
+		$dict = new skoro\stardict\Dict($index);
+		
+		$gap = time() - $start;
+		echo "{$gap}\n";
 
-// Lookup word.
-// print $dict->lookup('ведро');
-$end = time();
-echo "{$end}\n";
-echo "Look up for \'text\'\n";
-var_dump($dict->lookup('text'));
-$end = time();
-$gap = $end - $start;
-echo "{$gap}\n";
+		// $_SESSION['dict'] = $dict;
+		$_SESSION['dict'] = json_encode($dict);
+	}
+	
+	$dict = json_decode($_SESSION['dict']);
+
+	return $dict;
+};
+
+$search_term = isset($_GET['search_term']) ?  $_GET['search_term'] : 'hello';
+
+$dict = loadDict();
+var_dump((object)$dict->lookup($search_term));
+
 die;
